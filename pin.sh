35545429049c780e735ba04a500540a4518c4ea8
B9mkr <mushkaborys@gmail.com>
2020-12-03 17:05:29 +0100

    change script pin.sh

--- pin.sh
+++ pin.sh
@@ -1,19 +1,34 @@
 #! /bin/bash
 
+# Example:
+#
+# ./pin.sh 4 30s
+#
+# Repeat the ping command 4 times with an interval of 30 seconds
+
 i=0
-n=$1
-t=$2
+n=$1 # number of repetitions
+t=$2 # waiting time between pings
+pingUrl="www.google.com"
+sendingCount=10
 
 while [ $i -lt $n ]
 do
   i=$(( $i + 1 ))
-  echo "$i / $n, $[ $SECONDS / 60 ]"
-  date +%H:%M:%S
-  ping google.com
-  date +%H:%M:%S
+  data=`date +%H:%M:%S`
+  #echo "$i / $n, $[ $[ $SECONDS / 60 ] / 60 ] : $[ $SECONDS / 60 ] ; $data"
+
+  timeSpent=`date -ud "@$SECONDS" "+%H:%M:%S"`
+
+  echo "$i / $n ; Time spent: $timeSpent ; start ping: $data"
+  ping -c $sendingCount $pingUrl
+  data=`date +%H:%M:%S`
+  echo "                                    end ping: $data"
+  
   if [ $i -lt $n ]
   then
     sleep "$t"
   fi
 done
+
 
